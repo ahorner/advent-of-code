557feab292885b5493735ec016e19765989c6a28
Andrew Horner <andrew@tablexi.com>
2015-12-05 01:04:13 -0600

    Day 4: The Ideal Stocking Stuffer

---
+++ 04/code.rb
@@ -0,0 +1,15 @@
+require "digest"
+
+def matching_digest(pattern)
+  value = 0
+
+  begin
+    value += 1
+    digest = Digest::MD5.new.hexdigest "#{INPUT}#{value}"
+  end while !digest.start_with?(pattern)
+
+  return digest, value
+end
+
+puts *matching_digest("00000"), nil
+puts *matching_digest("000000")
