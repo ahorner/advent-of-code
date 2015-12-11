5f12e4d9db01ebee22514697aaee35c3c09bfaae
Andrew Horner <andrew@tablexi.com>
2015-12-10 23:38:05 -0600

    Day 11: Corporate Policy

---
+++ 11/code.rb
@@ -0,0 +1,45 @@
+class PasswordGenerator
+
+  def initialize(seed)
+    @current_password = seed
+  end
+
+  def next
+    password = @current_password
+
+    begin
+      password.succ!
+    end while !valid?(password)
+
+    @current_password = password
+  end
+
+  private
+
+  def valid?(password)
+    increasing_straight?(password) && two_pairs?(password) && !bad_chars?(password)
+  end
+
+  def increasing_straight?(string)
+    # Omitting the following, as they contain i, o, or l (see `bad_chars?`):
+    # ghi|hij|ijk|jkl|klm|lmn|mno|nop|opq
+    string =~ /abc|bcd|cde|def|efg|fgh|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz/
+  end
+
+  def two_pairs?(string)
+    string =~ /(.)\1.*(.)\2/
+  end
+
+  def bad_chars?(string)
+    string =~ /[iol]/
+  end
+
+end
+
+generator = PasswordGenerator.new(INPUT)
+
+next_password = generator.next
+puts "Next password should be:", next_password, nil
+
+next_password = generator.next
+puts "And then it should be:", next_password
