ad279848e5df0f69461bc12665526508aae0b388
Andrew Horner <andrew@tablexi.com>
2015-12-11 23:09:54 -0600

    Day 12: JSAbacusFramework.io

---
+++ 12/code.rb
@@ -0,0 +1,18 @@
+require "json"
+
+input = JSON.parse(INPUT)
+
+def deep_sum(object, ignore: nil)
+  case object
+  when Array
+    object.map { |i| deep_sum(i, ignore: ignore) }.inject(:+)
+  when Hash
+    return 0 if object.values.include?(ignore)
+    deep_sum(object.values, ignore: ignore)
+  else
+    object.to_i
+  end
+end
+
+puts "Total Value: ", deep_sum(input), nil
+puts "Total Adjusted Value: ", deep_sum(input, ignore: "red")
