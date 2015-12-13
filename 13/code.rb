9a4685f94f2afcd7a43f87d8e1692526b68f56f8
Andrew Horner <andrew@tablexi.com>
2015-12-13 00:47:39 -0600

    Day 13: Knights of the Dinner Table

---
+++ 13/code.rb
@@ -0,0 +1,76 @@
+require "set"
+
+class Party
+
+  INTERACTION_MATCHER =   /(?<person>.+) would (?<action>.+) (?<units>\d+) happiness units by sitting next to (?<neighbor>.+)\./
+
+  attr_reader :people
+
+  def initialize
+    @people = Set.new
+    @relationships = Hash.new(0)
+  end
+
+  def parse(guest_list)
+    guest_list.each do |line|
+      interaction = INTERACTION_MATCHER.match(line)
+
+      @people << interaction[:person]
+      @people << interaction[:neighbor]
+
+      units = interaction[:units].to_i
+      units *= -1 if interaction[:action] == "lose"
+
+      @relationships[[interaction[:person], interaction[:neighbor]]] = units
+    end
+  end
+
+  def seating_chart
+    @people.to_a.permutation.map do |arrangement|
+      SeatingChart.new(arrangement, @relationships)
+    end.max
+  end
+
+end
+
+class SeatingChart
+
+  def initialize(arrangement, relationships)
+    @relationships = relationships
+    @arrangement = arrangement
+  end
+
+  def happiness
+    @happiness ||= begin
+      total = 0
+
+      @arrangement.each.with_index do |person, index|
+        total += @relationships[[person, @arrangement[(index + 1) % @arrangement.size]]]
+        total += @relationships[[person, @arrangement[index - 1]]]
+      end
+
+      total
+    end
+  end
+
+  def <=>(other)
+    happiness <=> other.happiness
+  end
+
+  def to_s
+    (@arrangement + [@arrangement.first]).join(" - ")
+  end
+
+end
+
+party = Party.new
+party.parse(INPUT.split("\n"))
+
+seating_chart = party.seating_chart
+
+puts "Best arrangement:", seating_chart.to_s, seating_chart.happiness, nil
+
+party.people << "Me"
+seating_chart = party.seating_chart
+
+puts "Best arrangement (with me):", seating_chart.to_s, seating_chart.happiness
