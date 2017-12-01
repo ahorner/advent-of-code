f8ebe3ab86e7b6fc40079f099fef51a6ada4f4d2
Andrew Horner <andrew@tablexi.com>
2017-11-30 22:13:28 -0700

    Day 1: Inverse Captcha

---
+++ 2017/01/code.rb
@@ -0,0 +1,14 @@
+SEQUENCE = INPUT.split("").map(&:to_i)
+
+def sum(sequence, gap = 1)
+  sum = 0
+  sequence.each_with_index do |i, j|
+    comparison = (j + gap) % sequence.length
+    sum += i if i == sequence[comparison]
+  end
+
+  sum
+end
+
+puts "The sum of matching numbers is:", sum(SEQUENCE), nil
+puts "The sum of matching numbers (with a gap) is:", sum(SEQUENCE, SEQUENCE.length / 2)
