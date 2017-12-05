2b145aded911d1498d9cacf65fc89fbbacaddc0e
Andrew Horner <andrew@tablexi.com>
2017-12-04 22:11:59 -0700

    Day 5: A Maze of Twisty Trampolines, All Alike

---
+++ 2017/05/code.rb
@@ -0,0 +1,27 @@
+LIST = INPUT.split("\n").map(&:to_i)
+
+def steps(list)
+  cursor = 0
+  count = 0
+
+  while cursor >= 0 && cursor < list.length
+    current = cursor
+    count += 1
+    cursor += list[cursor]
+    yield list, current
+  end
+
+  count
+end
+
+count = steps(LIST.dup) do |list, index|
+  list[index] += 1
+end
+
+puts "The number of steps to escape is:", count
+
+count = steps(LIST.dup) do |list, index|
+  list[index] += list[index] >= 3 ? -1 : 1
+end
+
+puts "The number of steps to escape (with decrementing) is:", count
