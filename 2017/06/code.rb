decc1062eb8c4af8d6ec4e7ec20adc97b077e8dc
Andrew Horner <andrew@tablexi.com>
2017-12-05 22:14:45 -0700

    Day 6: Memory Reallocation

---
+++ 2017/06/code.rb
@@ -0,0 +1,24 @@
+BLOCKS = INPUT.split(/\s+/).map(&:to_i)
+
+blocks = BLOCKS.dup
+seen_states = {}
+count = 0
+
+while !seen_states.key?(blocks)
+  seen_states[blocks.dup] = count
+
+  cursor = blocks.index(blocks.max)
+  remaining = blocks[cursor]
+  blocks[cursor] = 0
+
+  while remaining > 0
+    cursor += 1
+    remaining -= 1
+    blocks[cursor % blocks.length] += 1
+  end
+
+  count += 1
+end
+
+puts "The number of cycles required to find an optimal balance is:", count, nil
+puts "The number of cycles between optimal states is:", count - seen_states[blocks]
