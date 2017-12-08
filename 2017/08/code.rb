fc9b22eb007cbf1a39b2772981a7421d3c7e7b86
Andrew Horner <andrew@tablexi.com>
2017-12-07 22:10:51 -0700

    Day 8: I Heard You Like Registers

---
+++ 2017/08/code.rb
@@ -0,0 +1,20 @@
+registers = Hash.new { |h, k| h[k] = 0 }
+maximum = 0
+
+LINE_PARSER = /(?<register>\w+) (?<instruction>.+) (?<value>-?\d+) if (?<cregister>\w+) (?<comparison>.+) (?<cvalue>-?\d+)/
+INPUT.split("\n").each do |row|
+  match = row.match(LINE_PARSER)
+  next unless registers[match[:cregister]].send(match[:comparison], match[:cvalue].to_i)
+
+  case match[:instruction]
+  when "inc"
+    registers[match[:register]] += match[:value].to_i
+  when "dec"
+    registers[match[:register]] -= match[:value].to_i
+  end
+
+  maximum = [maximum, registers[match[:register]]].max
+end
+
+puts "The highest value in a register after runtime is:", registers.values.max, nil
+puts "The highest value ever held in a register was:", maximum
