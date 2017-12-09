3354651f8303d1fab2811c019a9b7d6804e9585f
Andrew Horner <andrew@tablexi.com>
2017-12-08 22:30:57 -0700

    Day 9: Stream Processing

---
+++ 2017/09/code.rb
@@ -0,0 +1,36 @@
+STREAM = INPUT.split("")
+
+group_score = 0
+garbage_score = 0
+
+open_groups = 0
+garbage = false
+ignore = false
+
+STREAM.each do |c|
+  if garbage && ignore
+    ignore = false
+    next
+  end
+
+  garbage_score += 1 if garbage && !%w[> !].include?(c)
+
+  case c
+  when "<"
+    garbage = true
+  when ">"
+    garbage = false
+  when "!"
+    ignore = true if garbage
+  when "{"
+    next if garbage
+    open_groups += 1
+    group_score += open_groups
+  when "}"
+    next if garbage
+    open_groups -= 1 if open_groups > 0
+  end
+end
+
+puts "The number of points from groups is:", group_score, nil
+puts "The number of garbage characters is:", garbage_score
