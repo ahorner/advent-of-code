d0ee48adc0a7278015c4bc9d62c2d88fb2986761
Andrew Horner <andrew@tablexi.com>
2017-12-09 23:04:47 -0700

    Day 10: Knot Hash

---
+++ 2017/10/code.rb
@@ -0,0 +1,29 @@
+def tie_knot(lengths, rounds = 1)
+  list = (0..255).to_a
+  skip = 0
+  rotations = 0
+
+  rounds.times do
+    lengths.each do |length|
+      list[0...length] = list[0...length].reverse
+
+      list = list.rotate(length + skip)
+      rotations += length + skip
+      skip += 1
+    end
+  end
+
+  list.rotate(-rotations)
+end
+
+result = tie_knot(INPUT.split(",").map(&:to_i))
+knot_value = result[0] * result[1]
+
+puts "The check value of the knot is:", knot_value, nil
+
+MAGIC_NUMBERS = [17, 31, 73, 47, 23].freeze
+result = tie_knot(INPUT.bytes + MAGIC_NUMBERS, 64)
+knot_hash = result.each_slice(16).map { |block| format("%02x", block.inject(:"^")) }.join
+
+puts "The Knot Hash for the input is:", knot_hash
+
