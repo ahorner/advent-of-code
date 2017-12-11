975c065a769110c93677ca8dfcb00872d150ccad
Andrew Horner <andrew@tablexi.com>
2017-12-10 22:17:21 -0700

    Day 11: Hex Ed

---
+++ 2017/11/code.rb
@@ -0,0 +1,26 @@
+MOVEMENTS = INPUT.split(",")
+
+def distance(x, y, z)
+  (x.abs + y.abs + z.abs) / 2
+end
+
+x, y, z = [0, 0, 0]
+max = 0
+
+MOVEMENTS.each do |movement|
+  motion =
+    case movement
+    when "n" then [0, 1, -1]
+    when "ne" then [1, 0, -1]
+    when "se" then [1, -1, 0]
+    when "s" then [0, -1, 1]
+    when "sw" then [-1, 0, 1]
+    when "nw" then [-1, 1, 0]
+    end
+
+  x, y, z = [x, y, z].zip(motion).map { |c1, c2| c1 + c2 }
+  max = [max, distance(x, y, z)].max
+end
+
+puts "The distance to the child's final tile is:", distance(x, y, z), nil
+puts "The furthest distance at any point was:", max
