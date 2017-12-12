0c8dd6c997ee0d5e283edb83a129da5f7bf45191
Andrew Horner <andrew@tablexi.com>
2017-12-11 22:43:56 -0700

    Day 12: Digital Plumber

---
+++ 2017/12/code.rb
@@ -0,0 +1,32 @@
+programs = Hash.new { |h, k| h[k] = [] }
+
+INPUT.split("\n").each do |row|
+  program, friends = row.split(" <-> ")
+  friends = friends.split(", ")
+
+  posse = ([program] + friends)
+  posse.each do |item|
+    programs[item] = programs[item].concat(posse).uniq
+  end
+end
+
+def group(programs, index, seen = [])
+  seen = seen.dup
+
+  friends = programs[index] - seen
+  friends.each { |program| seen << program }
+  friends.each { |program| seen = group(programs, program, seen) }
+
+  seen
+end
+
+puts "The number of programs in 0's group is:", group(programs, "0").count, nil
+
+groups = []
+
+while programs.count > 0
+  groups << group(programs, programs.keys.first)
+  programs = programs.reject { |k, _| groups.last.include?(k) }
+end
+
+puts "The total number of groups is:", groups.count
