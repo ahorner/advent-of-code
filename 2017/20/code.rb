6e5b36941ac269ec02e16283fd2f4ab4ac184520
Andrew Horner <andrew@tablexi.com>
2017-12-19 23:07:03 -0700

    Day 20: Particle Swarm

---
+++ 2017/20/code.rb
@@ -0,0 +1,50 @@
+class Particle
+
+  PARTICLE_MATCHER = /p=\<(?<position>.+)\>, v=\<(?<velocity>.+)\>, a=\<(?<acceleration>.+)\>/
+
+  attr_reader :id, :position
+
+  def initialize(id, line)
+    @id = id
+
+    traits = PARTICLE_MATCHER.match(line)
+    @position = traits[:position].split(",").map(&:to_i)
+    @velocity = traits[:velocity].split(",").map(&:to_i)
+    @acceleration = traits[:acceleration].split(",").map(&:to_i)
+  end
+
+  def displacement
+    @position.map(&:abs).sum
+  end
+
+  def acceleration
+    @acceleration.map(&:abs).sum
+  end
+
+  def speed
+    @velocity.map(&:abs).sum
+  end
+
+  def tick!
+    @velocity = @velocity.zip(@acceleration).map(&:sum)
+    @position = @position.zip(@velocity).map(&:sum)
+  end
+
+end
+
+particles = []
+INPUT.split("\n").each_with_index do |line, i|
+  particles << Particle.new(i, line)
+end
+
+closest = particles.min_by { |p| [p.acceleration, p.speed, p.displacement] }
+puts "The long-term closest particle is:", closest.id, nil
+
+100.times do
+  particles.each(&:tick!)
+
+  counts = particles.each_with_object(Hash.new(0)) { |p, c| c[p.position] += 1 }
+  particles.reject! { |p| counts[p.position] > 1 }
+end
+
+puts "The number of particles remaining after collisions is:", particles.count
