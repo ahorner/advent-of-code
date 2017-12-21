6006a6c39307f057f55e294a1ce55c45aa25646c
Andrew Horner <andrew@tablexi.com>
2017-12-21 00:19:45 -0700

    Day 21: Fractal Art

---
+++ 2017/21/code.rb
@@ -0,0 +1,47 @@
+def variants(string)
+  base = string.split("/").map(&:chars)
+
+  [
+    base,
+    base.map(&:reverse),
+    base.reverse,
+    base.reverse.map(&:reverse),
+    base.transpose,
+    base.transpose.map(&:reverse),
+    base.transpose.reverse,
+    base.transpose.reverse.map(&:reverse),
+  ].uniq.map do |variant|
+    variant.map(&:join).join("/")
+  end
+end
+
+RULES = INPUT.split("\n").each_with_object({}) do |line, rules|
+  input, output = line.split(" => ")
+  variants(input).each { |variant| rules[variant] = output }
+end
+
+def enhance(grid)
+  section_size = grid.size.even? ? 2 : 3
+
+  grid.each_slice(section_size).flat_map do |lines|
+    portions = lines.map do |line|
+      line.chars.each_slice(section_size).map(&:join)
+    end.inject(&:zip)
+
+    results = portions.map { |p| RULES[p.join("/")].split("/") }
+    results.size > 1 ? results.inject(&:zip).map(&:join) : results.flatten
+  end
+end
+
+STARTING_GRID = [
+  ".#.",
+  "..#",
+  "###",
+]
+
+grid = STARTING_GRID.dup
+5.times { grid = enhance(grid) }
+puts "The number of pixels that stay on after 5 enhancements is:", grid.join.count("#"), nil
+
+13.times { grid = enhance(grid) }
+puts "The number of pixels that stay on after 18 enhancements is:", grid.join.count("#")
