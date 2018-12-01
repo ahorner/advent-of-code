35de00ac5656b87a13b7707d42b2b39a40569597
Andrew Horner <andrew@tablexi.com>
2018-12-01 09:19:39 -0700

    Day 1: Chronal Calibration

---
+++ 2018/01/code.rb
@@ -0,0 +1,16 @@
+CHANGES = INPUT.split("\n").map(&:to_i)
+
+puts "The final total frequency is:", CHANGES.sum, nil
+
+require "set"
+frequencies = Set.new
+frequency = nil
+
+loop do
+  change = CHANGES[frequencies.size % CHANGES.size]
+  frequency = frequency.to_i + change
+  break if frequencies.include?(frequency)
+  frequencies << frequency
+end
+
+puts "The first frequency achieved twice is:", frequency
