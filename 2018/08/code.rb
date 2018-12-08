3695a881a9b520c111bf932f6e32ed717fc4e9e9
Andrew Horner <andrew@tablexi.com>
2018-12-08 01:24:10 -0700

    Day 8: Memory Maneuver

---
+++ 2018/08/code.rb
@@ -0,0 +1,29 @@
+class Node
+  def initialize(children, metadata)
+    @children = children
+    @metadata = metadata
+  end
+
+  def sum
+    @children.sum(&:sum) + @metadata.sum
+  end
+
+  def value
+    return @metadata.sum if @children.none?
+    @metadata.sum { |i| @children[i - 1]&.value.to_i }
+  end
+end
+
+def node_for(definition)
+  child_count, metadata_count = definition.shift(2)
+
+  children = child_count.times.map { node_for(definition) }
+  metadata = definition.shift(metadata_count)
+
+  Node.new(children, metadata)
+end
+
+root = node_for(INPUT.split(" ").map(&:to_i))
+
+puts "The sum of all node metadata is:", root.sum, nil
+puts "The value of the root node is:", root.value
