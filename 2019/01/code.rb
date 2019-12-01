041424014dd84012a9b7ad67e87aed6c19f3b362
Andrew Horner <andrew@tablexi.com>
2019-11-30 22:18:56 -0700

    Day 1: The Tyranny of the Rocket Equation

---
+++ 2019/01/code.rb
@@ -0,0 +1,18 @@
+MODULES = INPUT.split("\n").map(&:to_i)
+
+def fuel_requirement(mass)
+  (mass / 3.0).floor - 2
+end
+
+total = MODULES.sum { |mass| fuel_requirement(mass) }
+puts "The total fuel requirement is:", total, "\n"
+
+def compounded_fuel_requirement(mass)
+  fuel = 0
+  fuel += mass = [fuel_requirement(mass), 0].max while mass > 0
+
+  fuel
+end
+
+total = MODULES.sum { |mass| compounded_fuel_requirement(mass) }
+puts "The total fuel requirement (with compounding) is:", total
