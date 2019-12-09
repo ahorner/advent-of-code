d5a50d47a1fd355d43c39dc4e0bfb3cb872226af
Andrew Horner <andrew@tablexi.com>
2019-12-08 22:32:19 -0700

    Day 9: Sensor Boost

--- 2019/02/code.rb
+++ 2019/02/code.rb
@@ -1,47 +1,23 @@
-INTCODE = INPUT.split(",").map(&:to_i).freeze
+require_relative "../shared/intcode"
 
-def process(program, pointer)
-  operation, advance =
-    case program[pointer]
-    when 1 then :+
-    when 2 then :*
-    when 99 then nil
-    else raise "Bad instruction at #{pointer} (#{program[pointer]})"
-    end
-
-  return nil unless operation
-
-  first_input = program[pointer + 1]
-  second_input = program[pointer + 2]
-  output = program[pointer + 3]
-
-  program[output] = program[first_input].public_send(operation, program[second_input])
-
-  pointer + 4
-end
-
-def run(intcode, noun, verb)
-  program = intcode.dup
+def run(noun, verb)
+  program = INTCODE.dup
   program[1] = noun
   program[2] = verb
 
-  pointer = 0
+  computer = Computer.new(program)
+  computer.run
 
-  loop do
-    pointer = process(program, pointer)
-    break unless pointer
-  end
-
-  program[0]
+  computer[0]
 end
 
-output = run(INTCODE, 12, 2)
+output = run(12, 2)
 puts "The output of the program is:", output, "\n"
 
 TARGET_OUTPUT = 19690720
 
 def search(intcode, target)
-  (0..99).each { |n| (0..99).each { |v| return [n, v] if run(intcode, n, v) == target } }
+  (0..99).each { |n| (0..99).each { |v| return [n, v] if run(n, v) == target } }
 end
 
 noun, verb = search(INTCODE, TARGET_OUTPUT)
--- 2019/07/code.rb
+++ 2019/07/code.rb
@@ -1,87 +1,4 @@
-INTCODE = INPUT.split(",").map(&:to_i).freeze
-
-class Memory
-  attr_reader :state
-
-  def initialize(state)
-    @state = state.dup
-  end
-
-  def instruction(pointer)
-    parameter = @state[pointer]
-
-    opcode = parameter % 100
-    modes = (2..4).map { |i| (parameter / (10 ** i)) % 10 }
-
-    [opcode, modes.zip([pointer + 1, pointer + 2, pointer + 3])]
-  end
-
-  def []((mode, register))
-    case mode
-    when 0 then @state[@state[register]]
-    when 1 then @state[register]
-    end
-  end
-
-  def []=((mode, register), value)
-    case mode
-    when 0 then @state[@state[register]] = value
-    when 1 then raise "Invalid storage mode!"
-    end
-  end
-end
-
-class Computer
-  def initialize(intcode)
-    @memory = Memory.new(intcode)
-    @pointer = 0
-    @running = true
-  end
-
-  def run(inputs: [])
-    outputs = []
-
-    loop do
-      opcode, arguments = @memory.instruction(@pointer)
-
-      case opcode
-      when 1
-        @memory[arguments[2]] = @memory[arguments[0]] + @memory[arguments[1]]
-        @pointer += 4
-      when 2
-        @memory[arguments[2]] = @memory[arguments[0]] * @memory[arguments[1]]
-        @pointer += 4
-      when 3
-        return outputs.last if inputs.none?
-
-        @memory[arguments[0]] = inputs.shift
-        @pointer += 2
-      when 4
-        outputs << @memory[arguments[0]]
-        @pointer += 2
-      when 5
-        @pointer = @memory[arguments[0]] != 0 ? @memory[arguments[1]] : @pointer + 3
-      when 6
-        @pointer = @memory[arguments[0]] == 0 ? @memory[arguments[1]] : @pointer + 3
-      when 7
-        @memory[arguments[2]] = @memory[arguments[0]] < @memory[arguments[1]] ? 1 : 0
-        @pointer += 4
-      when 8
-        @memory[arguments[2]] = @memory[arguments[0]] == @memory[arguments[1]] ? 1 : 0
-        @pointer += 4
-      when 99
-        @running = false
-        break
-      end
-    end
-
-    outputs.last
-  end
-
-  def running?
-    @running
-  end
-end
+require_relative "../shared/intcode"
 
 def amplify(phase_settings)
   signal = 0
@@ -90,7 +7,7 @@ def amplify(phase_settings)
   end
 
   loop do
-    amplifiers.each { |amplifier| signal = amplifier.run(inputs: [signal]) }
+    amplifiers.each { |amplifier| signal = amplifier.run(inputs: [signal]).last }
     break if amplifiers.none?(&:running?)
   end
 
