c64668e727113ecadcdcc14044b67a94c9d7bba4
Andrew Horner <andrew@tablexi.com>
2019-12-27 10:35:12 -0500

    Day 24: Planet of Discord

---
+++ 2019/24/code.rb
@@ -0,0 +1,73 @@
+SIZE = 5
+ADJACENCIES = [[0, -1, 0], [1, 0, 0], [0, 1, 0], [-1, 0, 0]].freeze
+GRID = INPUT.split("\n").each_with_index.each_with_object(Hash.new(".")) do |(line, y), grid|
+  line.split("").each_with_index { |tile, x| grid[[x, y, 0]] = tile }
+end
+
+def tick(grid, neighbors)
+  positions = grid.flat_map { |position, tile| tile == "#" ? [position] + neighbors.call(position) : [] }.uniq
+  positions.each_with_object(Hash.new(".")) do |position, updated|
+    bugs = neighbors.call(position).count { |neighbor| grid[neighbor] == "#" }
+    updated[position] =
+      case grid[position]
+      when "#" then bugs == 1 ? "#" : "."
+      when "." then bugs == 1 || bugs == 2 ? "#" : "."
+      end
+  end
+end
+
+def biodiversity(grid)
+  (0...SIZE).sum do |y|
+    (0...SIZE).sum do |x|
+      grid[[x, y, 0]] == "#" ? 2 ** (y * SIZE + x) : 0
+    end
+  end
+end
+
+def neighbors(coord)
+  x, y, z = coord
+  ADJACENCIES.map { |i, j, k| [x + i, y + j, z + k] }
+end
+
+grid = GRID.dup
+seen = {}
+neighbors = proc do |(x, y, z)|
+  ADJACENCIES.map do |i, j, k|
+    next if x + i < 0 || y + j < 0 || x + i >= SIZE || y + j >= SIZE
+    [x + i, y + j, z + k]
+  end.compact
+end
+
+repeated = loop do
+  grid = tick(grid, neighbors)
+  break grid if seen[grid]
+  seen[grid] = true
+end
+
+puts "The biodiversity of the first layout that appears twice is:", biodiversity(repeated), "\n"
+
+RECURSIVE_ADJACENCIES = {
+  [0, -1, 0] => -> (x) { [x, SIZE - 1] },
+  [1, 0, 0] => -> (y) { [0, y] },
+  [0, 1, 0] => -> (x) { [x, 0] },
+  [-1, 0, 0] => -> (y) { [SIZE - 1, y] },
+}.freeze
+
+recursive_neighbors = proc do |(x, y, z)|
+  ADJACENCIES.flat_map do |i, j, k|
+    if x + i == SIZE / 2 && y + j == SIZE / 2
+      SIZE.times.map(&RECURSIVE_ADJACENCIES[[i, j, k]]).map { |m, n| [m, n, z + 1] }
+    elsif x + i < 0 || y + j < 0 || x + i >= SIZE || y + j >= SIZE
+      [[SIZE / 2 + i, SIZE / 2 + j, z - 1]]
+    else
+      [[x + i, y + j, z + k]]
+    end
+  end
+end
+
+grid = GRID.dup
+200.times { grid = tick(grid, recursive_neighbors) }
+
+puts "The number of bugs after 200 minutes is:", grid.values.count("#")
+
+
