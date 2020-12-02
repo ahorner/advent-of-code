ae2ea1f6e4d6b0eda7f1c59d3091082c534c8010
Andrew Horner <andrew@tablexi.com>
2020-12-01 22:19:43 -0700

    Day 2: Password Philosophy

---
+++ 2020/02/code.rb
@@ -0,0 +1,19 @@
+POLICY_MATCHER = /(?<minimum>\d+)-(?<maximum>\d+) (?<char>\w)\: (?<password>\w+)/
+LINES = INPUT.split("\n").map { |line| line.match(POLICY_MATCHER) }
+
+def valid_by_count?(line)
+  count = line[:password].count(line[:char])
+  count >= line[:minimum].to_i && count <= line[:maximum].to_i
+end
+
+puts "The number of valid passwords is:",
+  LINES.count { |line| valid_by_count?(line) },
+  "\n"
+
+def valid_by_position?(line)
+  (line[:password][line[:minimum].to_i - 1] == line[:char]) ^
+    (line[:password][line[:maximum].to_i - 1] == line[:char])
+end
+
+puts "The number of valid passwords with the new interpretation is:",
+  LINES.count { |line| valid_by_position?(line) }
