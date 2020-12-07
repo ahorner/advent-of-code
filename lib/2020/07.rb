f7df8de87e06d3eed0be4219bf2da951967a402f
Andrew Horner <andrew@tablexi.com>
2020-12-07 08:16:24 -0700

    Day 7: Handy Haversacks

---
+++ lib/2020/07.rb
@@ -0,0 +1,80 @@
+require "set"
+
+OUTER_BAG_MATCHER = /^(?<color>.+) bags contain (?<contents>.+)\.$/.freeze
+EMPTY_BAG_MATCHER = /^no other bags$/.freeze
+INNER_BAG_MATCHER = /^(?<count>\d+) (?<color>.+) bags?/.freeze
+
+STARTING_BAG = "shiny gold".freeze
+
+class Bag
+  attr_reader :color, :contents
+
+  def initialize(color)
+    @color = color
+    @contents = {}
+  end
+
+  def holds!(color, count)
+    @contents[color] = count
+  end
+
+  def holds?(contents)
+    case contents
+    when Bag then @contents.key?(contents.color)
+    when String then @contents.key?(contents)
+    when Array then contents.any? { |bag| holds?(bag) }
+    else false
+    end
+  end
+
+  def empty?
+    @contents.empty?
+  end
+end
+
+BAGS = INPUT.split("\n").each_with_object({}) do |line, bags|
+  description = line.match(OUTER_BAG_MATCHER)
+  color = description[:color]
+  bags[color] = Bag.new(color)
+
+  next if description[:contents].match?(EMPTY_BAG_MATCHER)
+
+  description[:contents].split(", ").each do |bag|
+    inner_bag = bag.match(INNER_BAG_MATCHER)
+    bags[color].holds!(inner_bag[:color], inner_bag[:count].to_i)
+  end
+end
+
+def valid_containers(starting_bag)
+  containers = Set.new
+  new_bags = [starting_bag]
+
+  loop do
+    new_bags = BAGS.values.select { |bag| bag.holds?(new_bags) }
+    break if new_bags.empty?
+
+    containers += new_bags
+  end
+
+  containers
+end
+
+count = valid_containers(STARTING_BAG).count
+solve!("The number of valid outer bags is:", count)
+
+def contents_for(bag, memo = {})
+  return memo[bag.color] if memo.key?(bag.color)
+
+  contents = Hash.new(0)
+  bag.contents.each do |color, count|
+    contents[color] += count
+    contents_for(BAGS[color], memo).each do |c, n|
+      contents[c] += n * count
+    end
+  end
+
+  memo[bag.color] = contents
+end
+
+count = contents_for(BAGS[STARTING_BAG]).values.sum
+solve!("The total number of bags in our bag is:", count)
---
+++ spec/2020/07_spec.rb
@@ -0,0 +1,52 @@
+require "spec_helper"
+
+describe "Day 7: Handy Haversacks" do
+  let(:runner) { Runner.new("2020/07") }
+  let(:input) do
+    <<~TXT
+      light red bags contain 1 bright white bag, 2 muted yellow bags.
+      dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+      bright white bags contain 1 shiny gold bag.
+      muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+      shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+      dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+      vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+      faded blue bags contain no other bags.
+      dotted black bags contain no other bags.
+    TXT
+  end
+
+  describe "Part One" do
+    let(:solution) { runner.execute!(input, part_one: true)[0] }
+
+    it "determines the correct number of outermost bags" do
+      expect(solution).to eq(4)
+    end
+  end
+
+  describe "Part Two" do
+    let(:solution) { runner.execute!(input)[1] }
+
+    it "determines how many bags your bag contains" do
+      expect(solution).to eq(32)
+    end
+
+    describe "for a different list of bags" do
+      let(:input) do
+        <<~TXT
+          shiny gold bags contain 2 dark red bags.
+          dark red bags contain 2 dark orange bags.
+          dark orange bags contain 2 dark yellow bags.
+          dark yellow bags contain 2 dark green bags.
+          dark green bags contain 2 dark blue bags.
+          dark blue bags contain 2 dark violet bags.
+          dark violet bags contain no other bags.
+        TXT
+      end
+
+      it "determines how many bags your bag contains" do
+        expect(solution).to eq(126)
+      end
+    end
+  end
+end
