cd117b087612a3a767e7f2278290cebd6c80d6ef
Andrew Horner <andrew@tablexi.com>
2021-12-04 12:50:51 -0700

    Day 4: Giant Squid

---
+++ spec/2021/04_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper"
+
+RSpec.describe "Day 4: Giant Squid" do
+  let(:runner) { Runner.new("2021/04") }
+  let(:input) do
+    <<~TXT
+      7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+      22 13 17 11  0
+       8  2 23  4 24
+      21  9 14 16  7
+       6 10  3 18  5
+       1 12 20 15 19
+
+       3 15  0  2 22
+       9 18 13 17  5
+      19  8  7 25 23
+      20 11 10 24  4
+      14 21 16 12  6
+
+      14 21 17 24  4
+      10 16 15  9 19
+      18  8 23 26 20
+      22 11 13  6  5
+       2  0 12  3  7
+    TXT
+  end
+
+  describe "Part One" do
+    let(:solution) { runner.execute!(input, part: 1) }
+
+    it "calculates the score of the winning board" do
+      expect(solution).to eq(4512)
+    end
+  end
+
+  describe "Part Two" do
+    let(:solution) { runner.execute!(input, part: 2) }
+
+    it "calculates the score of the losing board" do
+      expect(solution).to eq(1924)
+    end
+  end
+end
